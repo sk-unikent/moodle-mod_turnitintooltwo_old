48772ff51b84677b76b2817b72bbd7cb8a975813
Skylar Kelty <s.kelty@kent.ac.uk>
2016-03-08 09:05:20 +0000

    Add search strings

--- lang/en/turnitintooltwo.php
+++ lang/en/turnitintooltwo.php
@@ -3,6 +3,7 @@
 // Date: 2015-04-01
 // Language: en
 
+$string['search:activity'] = 'Turnitintooltwo activities';
 $string['addsubmission'] = 'Add Submission';
 $string['allowlate'] = 'Allow Submissions after the Due Date';
 $string['allsubmissions'] = 'Submission Inbox';
